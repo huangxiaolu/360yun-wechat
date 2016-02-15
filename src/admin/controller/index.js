bcd38d81341916dd2115e77d87959a07ecd6913d
Cathy Huang <lu.huang90@gmail.com>
2016-02-15 15:00:27 +0800

    wechat admin

---
+++ src/admin/controller/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import Base from './base.js';
+export default class extends Base {
+  /**
+   * index action
+   * @return {Promise} []
+   */
+    indexAction() {
+      this.display();
+    }
+    addAction() {
+      this.display();
+    }
+    deleteAction() {
+
+    }
+    updateAction() {
+
+    }
+
+}
